5bb309da6ad087975d1b6b6b96f332c70e6fba26
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-03-31 21:55:08 -0400

    Added color variables and dark mode

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,4 +1,8 @@
 function darkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+  var e = document.body;
+  e.classList.toggle("dark-mode");
+
+  var element = document.getElementById("darkMode");
+  element.classList.remove("fa fa-moon");
+  element.classList.add("fa fa-sun");
 }
